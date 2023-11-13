1e390f9ca55d4cde7b5a470036c8484d94170b52
mlunarc01 <150673226+mlunarc01@users.noreply.github.com>
2023-11-13 12:34:39 +0100

    Add files via upload

---
+++ ProcesoPorLotes/mkprofiles.bat
@@ -0,0 +1,11 @@
+REM Script que genera un árbol de directorios 
+REM Manuel Lunar Cabeza
+REM Fecha: 13-11-2023
+
+@echo off
+mkdir %USERPROFILE%\asir mkdir %USERPROFILE%\dam mkdir %USERPROFILE%\smr mkdir %USERPROFILE%\daw mkdir %USERPROFILE%\ceti
+mkdir %USERPROFILE%\dam\pract mkdir %USERPROFILE%dam\docs %USERPROFILE%dam\exam
+mkdir %USERPROFILE%asir\docs mkdir %USERPROFILE%asir\docs mkdir %USERPROFILE%asir\docs  
+mkdir %USERPROFILE%smr\docs mkdir %USERPROFILE%smr\docs mkdir %USERPROFILE%smr\docs 
+mkdir %USERPROFILE%daw\docs mkdir %USERPROFILE%daw\docs mkdir %USERPROFILE%daw\docs 
+mkdir %USERPROFILE%ceti\docs mkdir %USERPROFILE%ceti\docs mkdir %USERPROFILE%ceti\docs 
